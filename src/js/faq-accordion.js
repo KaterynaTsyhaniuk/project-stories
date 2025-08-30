ea15a48e9e5b9be2c8bbee6736d0400643cda4a8
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2025-08-30 23:43:37 +0300

    add faq section

---
+++ src/js/faq-accordion.js
@@ -0,0 +1,10 @@
+import Accordion from 'accordion-js';
+
+const aboutMeAccordion = new Accordion('.acordeon-list-faq', {
+  duration: 400,
+  showMultiple: true,
+  openOnInit: [0],
+  elementClass: 'acordeon-item-faq',
+  triggerClass: 'block-faq',
+  panelClass: 'js-acordion-panel',
+});
--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
 import './js/gallery-swiper';
+import './js/faq-accordion';
